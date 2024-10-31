02820ac941d7294e56189777d8781adf26546fb2
Jan Hvid Sørensen <sorensenjan@gmail.com>
2024-10-31 07:51:56 +0100

    install script

--- karabiner/install.sh
+++ karabiner/install.sh
@@ -8,3 +8,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 jsonnet "$DIR/my-rules.jsonnet" > "$DIR/my-rules.json"
 
 cp -v "$DIR/my-rules.json" ~/.config/karabiner/assets/complex_modifications
+cp -v "$DIR/my-rules.json" ~/.config/karabiner/karabiner.json
+# /Library/Application\ Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli --reload-karabiner-json
